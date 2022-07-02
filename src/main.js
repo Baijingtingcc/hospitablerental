5e1727fbb6537251329760b8cb4bcc54b0b40431
Good Bai <1951433682@qq.com>
2022-07-02 15:12:40 +0800

    完成了初始化

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-
+import router from './routers'
+import vant from '@/plugins/vant'
 Vue.config.productionTip = false
-
+Vue.use(vant)
 new Vue({
   router,
   render: h => h(App)
